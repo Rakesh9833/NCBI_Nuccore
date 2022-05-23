fc1316980920111e7899c01f8358d44609114711
Rakesh9833 <105837651+Rakesh9833@users.noreply.github.com>
2022-05-22 19:35:59 -0500

    Update Nuccore_ETL.py

--- Nuccore_ETL.py
+++ Nuccore_ETL.py
@@ -17,6 +17,7 @@ from sql_helpers import sql_connector
 
 es_ = elastic_upload()
 mycursor, mydb = sql_connector()
+import mysql.connector
 
 
 
@@ -46,19 +47,11 @@ def fetch_data_from_id(id, query):
 
 
 
-def store_json_data(result):
-    save_to_json('nuccore_data.json', result)
-
-
-def store_id_data(result):
-    save_to_json('nuccore_id_data.json', result)
-
-
 
 @task
 def fetch_data_from_query(query):
     query_response = requests.get(
-            'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=nuccore&retmode=json&tool=rex-ncbi&term=%s&retmax=100' % str(query)).json()
+            'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=nuccore&retmode=json&tool=rex-ncbi&term=%s&retmax=4' % str(query)).json()
     nuccore_list = query_response['esearchresult']['idlist']     
 
     all_data = []
@@ -81,8 +74,9 @@ def fetch_data_from_query(query):
         all_data.append([i, output[0], output[1], output[2] , output[3]])
         
     #es_.index(index="id-index1", id=1, document=json.dumps(ids_json))
-    # helpers.bulk(es_, json_dataset, index='gds-data',)
-    # # es_.index(index="gds-data", id=2, document=json.dump(json_dataset))
+    #helpers.bulk(es_, json_dataset, index='nuccore-data',)
+    #es_.index(index="nuccore-data", id=2, document=json.dump(json_dataset))
+
     store_id_data(ids_json)
     store_json_data(json_dataset)
     return all_data
@@ -90,23 +84,53 @@ def fetch_data_from_query(query):
 @task
 def store_ncbi_data(parsed):
     create_script = 'CREATE TABLE IF NOT EXISTS nuccore_table (id INT, title TEXT, source TEXT, organism TEXT, taxonomy TEXT)'
-    insert_cmd = "INSERT INTO gds_table VALUES (?, ?, ?, ?, ?)"
+    insert_cmd = "INSERT INTO nuccore_table VALUES (?, ?, ?, ?, ?)"
+    select_cmd = "SELECT * FROM nuccore_table"
 
-    with closing(sqlite3.connect("nuccore_data.db")) as conn:
+
+    with closing(sqlite3.connect("prefect_test.db")) as conn:
         with closing(conn.cursor()) as cursor:
             cursor.executescript(create_script)
             cursor.executemany(insert_cmd, parsed)
+            a = cursor.execute(select_cmd)
+            conn.commit()
+	
+            
+            #Delete the duplicate data (deduplicate)
+            cursor.execute('''DELETE FROM nuccore_table WHERE EXISTS 
+            ( SELECT 1 FROM nuccore_table t2 WHERE nuccore_table.id = t2.id AND nuccore_table.rowid > t2.rowid)''')
             conn.commit()
 
+
+            # Display data inserted -- test done for only 4 ids 
+            print("Data Inserted in the table: ")
+            data=cursor.execute('''SELECT * FROM nuccore_table''')
+            for row in data:
+                print(row)
+        
+ 
+
+def store_json_data(result):
+    save_to_json('nuccore_data.json', result)
+
+
+def store_id_data(result):
+    save_to_json('nuccore_id_data.json', result)
 
 
 with Flow("nuccore-flow") as flow:
     query = Parameter('NCBI', default='NCBI')
     parsed = fetch_data_from_query(query)
-    # store_ncbi_data(parsed)
+    store_ncbi_data(parsed)
 
 flow.run()
 flow.register(project_name="prefect_Assignment")
+
+
+#for fasta files 
+#conda install -c bioconda ncbi-genome-download
+#ncbi-genome-download --formats fasta,assembly-report viral
+
 
 #For docker
 #built_storage = flow.storage.build(push=False)
